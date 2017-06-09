d8c3d3467e4c4cf86b06ceb0960b18d316e2e4c1
Brandon Cooke <brandoncookedev@gmail.com>
2017-06-09 15:24:58 -0400

    Added cors to the server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var co  = require('co');
 var log = require('./lib/Logger');
+
 const express = require('express');
 var app = express();
+var cors = require('cors');
 var bodyParser = require('body-parser');
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
